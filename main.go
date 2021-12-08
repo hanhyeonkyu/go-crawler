bebc39c5c81103f9b5a2b55b83847c94df489999
aijialex <aiji.alexhan@gmail.com>
2021-12-08 11:08:39 +0900

    feat: first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func basic1() {
+	// get page
+	doc, docErr := goquery.NewDocument("https://modu-print.tistory.com/449?category=863372")
+	if docErr != nil {
+		fmt.Println(docErr.Error())
+		return
+	}
+	// get id
+	codeArea := doc.Find("#code_1606220695117")
+	// if it is
+	if codeArea != nil {
+		fmt.Println(codeArea.Text())
+		fmt.Println(codeArea.Html())
+	}
+}
+
+func main() {
+	basic1()
+}
