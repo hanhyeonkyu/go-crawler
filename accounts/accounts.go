93435049b8fd1d5f4f44474ef3b2a49f3205da65
aijialex <aiji.alexhan@gmail.com>
2022-01-04 10:19:25 +0900

    feat: additional case

---
+++ accounts/accounts.go
@@ -0,0 +1,15 @@
+package accounts
+
+type Account struct {
+	owner   string
+	balance int
+}
+
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+func (a Account) Deposit(amount int) {
+
+}
--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
+	"dev/go-crawler/accounts"
 	"fmt"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
-func basic1() {
-	// get page
-	doc, docErr := goquery.NewDocument("https://modu-print.tistory.com/449?category=863372")
-	if docErr != nil {
-		fmt.Println(docErr.Error())
-		return
-	}
-	// get id
-	codeArea := doc.Find("#code_1606220695117")
-	// if it is
-	if codeArea != nil {
-		fmt.Println(codeArea.Text())
-		fmt.Println(codeArea.Html())
-	}
-}
-
 func main() {
-	basic1()
+	account := accounts.NewAccount("alex")
+	fmt.Println(account)
 }
